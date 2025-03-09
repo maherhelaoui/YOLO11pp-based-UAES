7c686c573d323e69f5fb5127365b9d81576c1340
maherhelaoui <maher.helaoui@gmail.com>
2025-03-09 21:57:14 +0100

    Update DemoUADS.py

--- DemoUADS.py
+++ DemoUADS.py
@@ -1,3 +1,17 @@
+#If you use this demo or Universal Autonomous Driving System software from this repository in your work, 
+#please cite it using the following format:
+
+#BibTeX 
+#@article{hel25YOLO11++UAES, 
+#title={Universal Autonomous Driving System: Innovation with YOLO11++-based Universal Autonomous Expert System}, 
+#author={Maher, Helaoui and Sahbi, Bahroun and Ezzeddine, Zagrouba}, 
+#journal={The Visual Computer (Submitted)}, 
+#url = {https://github.com/maherhelaoui/YOLO11pp-based-UAES/}, 
+#year={2025}, publisher={Springer} }
+
+Please note that the DOI will be added to the citation once it is available.
+
+
 import os
 import shutil
 from pathlib import Path
